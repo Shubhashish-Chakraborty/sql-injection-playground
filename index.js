6cde12c2c8dc907b367ed79bc6955cf2ec8a5d09
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2026-04-19 14:39:51 +0530

    deployed

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-require('dotenv').config();
-const express = require("express");
-const { Client } = require("pg");
+require('dotenv').config()
+const express = require('express');
+const { Client } = require('pg');
 const app = express();
 const PORT = 3000;
-const DB_URL = process.env.DB_URL;
 app.use(express.json());
 
-const pgc = new Client(DB_URL)
-pgc.connect(); // db connection established!
 
-app.get("/" , async (req,res) => {
-    const response = await pgc.query(`SELECT * FROM users;`);
+const pgc = new Client(process.env.DATABASE_URL);
+pgc.connect();
+
+app.get("/" , (req, res) => {
     res.json({
-        rowcount: response.rowCount,
-        data: response.rows
-    });
+        message: "Server is up and running!!!"
+    })
 })
 
-// app.post('/' , async(req,res) => {
-//     const response = await pgc.query("DELETE FROM users;");
-//     res.json({message: "deleted!"});
-// })
-
-app.get("/data", async (req,res) => {
+app.get("/users" , async (req, res) => {
     const response = await pgc.query("SELECT * FROM users;");
     res.json(response.rows);
 })
@@ -30,13 +23,12 @@ app.get("/data", async (req,res) => {
 app.post("/add" , async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-
-    const sqlQuery = `INSERT INTO users (username, password) VALUES ('${username}', '${password}');`;
-
+    const sqlQuery = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
     const response = await pgc.query(sqlQuery);
-    res.send(response);
+    res.json(response);
 })
 
-app.listen(PORT,() => {
-    console.log(`http://localhost:${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`backend is up at http://localhost:${PORT}`)
 })
